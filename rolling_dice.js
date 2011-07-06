342af78e6eafd116349fa4cf954007f8babd7c54
Simon <kilik2049@gmail.com>
2011-07-06 23:09:00 +0200

    The Forgotten Commit

--- rolling_dice.js
+++ rolling_dice.js
@@ -1,28 +1,29 @@
+var j=1;
+
 function rollDice()
 {
   var numberRoll = document.getElementById('number').value.match(/^\d+$/); 
   var type_dice = document.getElementById('type').value.match(/^\d+$/);
+  var result = document.getElementById('result');
   
   if (numberRoll == null || type_dice == null) {
    alert('The fields require numbers, you fool !');
   } else {
-  
+    result.innerHTML += "Result of dice 1: ";
      for(var i = 0; i < numberRoll; i++) {
         // Make dah roll !
         var roll = 1+Math.floor(Math.random()*type_dice);
         // Display the result
-        document.getElementById('result').innerHTML += roll + " ";
+       result.innerHTML += roll + " ";
      }
-    document.getElementById('result').innerHTML += "<br />";
+    result.innerHTML += "<br />";
   
   }
 }
 
-var j=1;
 function addDice()
 {
-
   j++;
-  document.getElementById('increment').innerHTML+="Type of dice "+j+": <span id='increment'></span><input type=\"text\" id=\"type\" /><br />";
-  document.getElementById('addRoll').innerHTML += "Number of roll, dice "+j+": <span id='addRoll'></span><input type=\"text\" id=\"number\" /><br />";
+  document.getElementById('addDice').innerHTML+="<label>Type of dice "+j+":</label> <span id='increment'></span><input type='text' id='type' /><br />";
+  document.getElementById('addRoll').innerHTML += "<label>Number of roll, dice "+j+":</label> <span id='addRoll'></span><input type='text' id='number' /><br />";
 }
